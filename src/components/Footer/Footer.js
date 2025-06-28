902184e667abba90cc11fe352603a9b489c280fa
Brainless-Loco <dev.tcdas@gmail.com>
2025-06-29 02:38:45 +0600

    Footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,18 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export default function Footer() {
   return (
-    <Box className="flex min-h-14 bg-cyan-900 text-white justify-center items-center">
-        <CopyrightIcon className='mr-2'/> BIKE Lab 204
+    <Box className="flex min-h-14 bg-cyan-900 text-white justify-center items-center space-x-2">
+      <CopyrightIcon className="mr-2" />
+      <span>BIKE Lab 2024-2025 | Developed by</span>
+      <a
+        href="https://github.com/Brainless-Loco"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline text-white hover:text-cyan-300"
+      >
+        Brainless Loco
+      </a>
     </Box>
+
   )
 }
